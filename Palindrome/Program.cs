cf5400b56b761dd3126b2e93effc279bb1280252
NickWolfen99 <strftwolfen99@gmail.com>
2019-05-20 22:03:41 +0300

    Initial commit

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -10,6 +10,31 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your word:");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(Plndrm(word));
+            Console.ReadLine();
+        }
+        public static bool Plndrm(string word)
+        {
+            int i = 0;
+            int j = word.Length - 1;
+            while (true)
+            {
+                if (i > j)
+                {
+                    return true;
+                }
+                char a = word[i];
+                char b = word[j];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
         }
     }
 }
